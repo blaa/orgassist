c5378c22bc9e80a012b6e7d3a08d8a6bc3fa4215
Tomasz bla Fortuna <bla@thera.be>
2018-05-19 10:47:43 +0200

    Command dispatch works.

--- orgassist/__init__.py
+++ orgassist/__init__.py
@@ -3,3 +3,6 @@
 from .config import Config
 from .xmpp_bot import XmppBot
 from .assistant import Assistant
+
+# Register commands
+from . import commands_org
--- orgassist/assistant.py
+++ orgassist/assistant.py
@@ -1,14 +1,32 @@
+import functools
 from . import templates
 
 class Assistant:
     """
     """
+    # [([command1, command2], Handler), ...)
+    handler_classes = []
 
     def __init__(self, name, cfg, scheduler):
         self.scheduler = scheduler
         self.name = name
         self.cfg = cfg
+        self.initialize_handlers()
         self._validate_config()
+
+    @classmethod
+    def register_handler(cls, names, handler):
+        "Register assistant command"
+        cls.handler_classes.append((names, handler))
+
+    def initialize_handlers(self):
+        "Create instances of command handlers"
+        # {name: handler1, name2: handler1, name3: handler2, ...}
+        self.handlers = {}
+        for names, Handler in Assistant.handler_classes:
+            handler = Handler(self, self.scheduler)
+            for name in names:
+                self.handlers[name] = handler
 
     def _validate_config(self):
         "Simple config validation - fail early"
@@ -32,4 +50,32 @@ class Assistant:
         """
         print("GOT MESSAGE!", respond, sender, message)
 
-        respond(templates.get('DONT_UNDERSTAND'))
+        command = message.split()[0]
+        command = command.lower().strip()
+        print()
+        print("HH", self.handlers)
+        handler = self.handlers.get(command, None)
+        if handler is not None:
+            handler.message(respond, sender, message)
+        else:
+            respond(templates.get('DONT_UNDERSTAND'))
+
+
+class CommandHandler:
+    """
+    Command handler interface
+    """
+    def __init__(self, assistant, scheduler):
+        self.assistant = assistant
+        self.scheduler = scheduler
+
+    def message(self, respond, sender, message):
+        raise NotImplementedError
+
+# Decorator to register command handler
+def command(names):
+    # Register
+    def decorator(handler_cls):
+        Assistant.register_handler(names, handler_cls)
+        return handler_cls
+    return decorator
